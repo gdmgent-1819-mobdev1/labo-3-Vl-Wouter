12b6a50cf48790676c4521423260fcf7d426be49
gdm-1819-woutvlae <woutvlae@student.arteveldehs.be>
2018-10-10 14:25:46 +0200

    Initial Commit

---
+++ Readme.md
@@ -0,0 +1,12 @@
+# Tinder v2 (Wouter Vlaeyen)
+
+## Todo
+
+* [x] Add map
+* [ ] Get user coordinates
+* [ ] Store user coordinates in localstorage
+* [ ] Include Tinder v1 functionality
+* [ ] Update styling
+* [ ] ...
+
+Labo 3 - MobDev-I
---
+++ js/main.js
@@ -0,0 +1,5 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoidmwtd291dGVyIiwiYSI6ImNqbjMzZmF3aTAwYmMza29jd3M1NXkycnAifQ.rnu6svUUZZiURmBMZzIjAQ';
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v9'
+    });

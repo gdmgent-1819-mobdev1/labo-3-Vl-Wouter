d4cf9e6e9a047b0e3dae2a0d6c0d2d3ed86bf230
gdm-1819-woutvlae <woutvlae@student.arteveldehs.be>
2018-10-10 14:39:17 +0200

    Map works

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 mapboxgl.accessToken = 'pk.eyJ1Ijoidmwtd291dGVyIiwiYSI6ImNqbjMzZmF3aTAwYmMza29jd3M1NXkycnAifQ.rnu6svUUZZiURmBMZzIjAQ';
     var map = new mapboxgl.Map({
         container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v9'
+        style: 'mapbox://styles/mapbox/streets-v9',
+        zoom: 0
     });
